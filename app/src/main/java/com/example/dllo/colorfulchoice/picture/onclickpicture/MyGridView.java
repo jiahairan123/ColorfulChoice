bebbccc539c282a93de804dbb52fe0591a91c66e
贾海然 <915386240@qq.com>
2016-09-23 11:08:15 +0800

    PictureFragment over

---
+++ app/src/main/java/com/example/dllo/colorfulchoice/picture/onclickpicture/MyGridView.java
@@ -0,0 +1,33 @@
+package com.example.dllo.colorfulchoice.picture.onclickpicture;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * Coder: JiaHaiRan
+ * created on 16/9/23 10:59
+ */
+
+public class MyGridView extends GridView {
+    public MyGridView(Context context) {
+        super(context);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        // TODO 自动生成的构造函数存根
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // TODO 自动生成的方法存根
+        int expandSpec = MeasureSpec.makeMeasureSpec(
+                Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
